626d58732b53933166e2454ff8489d82fa961aad
Eriko eMac <nanonanoyooo@gmail.com>
2024-07-11 14:20:33 +0900

    gitignore firebase

--- src/api/news.tsx
+++ src/api/news.tsx
@@ -1,24 +1,20 @@
-const apiKey = "bb3dc1bd950345bc889e8d9705a18803";
+import { doc, getDoc } from "firebase/firestore";
 
 export interface Article {
-  id: number;
+  id: string;
   title: string;
   publishedAt: string;
   author: string;
   imageUrl?: string;
   content: string;
 }
-export async function fetchNewsArticles(): Promise<Article[]> {
-  const response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=ja&apiKey=${apiKey}`,
-  );
-  const data = await response.json();
-  return data.articles.map((article: any) => ({
-    id: article.source.id,
-    title: article.title,
-    publishedAt: article.publishedAt,
-    author: article.author,
-    imageUrl: article.urlToImage,
-    content: article.content,
-  }));
+
+const docRef = doc(db, "cities", "SF");
+const docSnap = await getDoc(docRef);
+
+if (docSnap.exists()) {
+  console.log("Document data:", docSnap.data());
+} else {
+  // docSnap.data() will be undefined in this case
+  console.log("No such document!");
 }
---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
