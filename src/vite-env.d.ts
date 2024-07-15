f00a4de747b7c763370baf17013bf7eb471946e9
Eriko eMac <nanonanoyooo@gmail.com>
2024-07-16 00:01:57 +0900

    環境変数の型定義

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_NEWS_API_KEY: string;
+  VITE_FIREBASE_APIKEY: string;
+  VITE_FIREBASE_DOMAIN: string;
+  VITE_FIREBASE_DATABASE: string;
+  VITE_FIREBASE_PROJECT_ID: string;
+  VITE_FIREBASE_STORAGE_BUCKET: string;
+  VITE_FIREBASE_SENDER_ID: string;
+  VITE_FIREBASE_APP_ID: string;
+  VITE_FIREBASE_MEASUREMENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
